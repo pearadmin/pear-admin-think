00419dca92264350a377efed39b8ab98c0c875c2
大梦 <muyiboss@qq.com>
2021-07-18 12:32:08 +0800

    增加Excel导出

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -71,6 +71,15 @@ private static $installed = array (
       ),
       'reference' => '4ef4c7815cf928392eecdb8cf7fd3ae45e6e3f59',
     ),
+    'ezyang/htmlpurifier' => 
+    array (
+      'pretty_version' => 'v4.13.0',
+      'version' => '4.13.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '08e27c97e4c6ed02f37c5b2b20488046c8d90d75',
+    ),
     'league/flysystem' => 
     array (
       'pretty_version' => '1.1.4',
@@ -98,6 +107,42 @@ private static $installed = array (
       ),
       'reference' => '3b9dff8aaf7323590c1d2e443db701eb1f9aa0d3',
     ),
+    'maennchen/zipstream-php' => 
+    array (
+      'pretty_version' => '2.1.0',
+      'version' => '2.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c4c5803cc1f93df3d2448478ef79394a5981cc58',
+    ),
+    'markbaker/complex' => 
+    array (
+      'pretty_version' => '2.0.3',
+      'version' => '2.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6f724d7e04606fd8adaa4e3bb381c3e9db09c946',
+    ),
+    'markbaker/matrix' => 
+    array (
+      'pretty_version' => '2.1.3',
+      'version' => '2.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '174395a901b5ba0925f1d790fa91bab531074b61',
+    ),
+    'myclabs/php-enum' => 
+    array (
+      'pretty_version' => '1.8.3',
+      'version' => '1.8.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b942d263c641ddb5190929ff840c68f78713e937',
+    ),
     'nesbot/carbon' => 
     array (
       'pretty_version' => '2.50.0',
@@ -116,6 +161,15 @@ private static $installed = array (
       ),
       'reference' => 'a5b5c43e50b7fba655f793ad27303cd74c57363c',
     ),
+    'phpoffice/phpspreadsheet' => 
+    array (
+      'pretty_version' => '1.18.0',
+      'version' => '1.18.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '418cd304e8e6b417ea79c3b29126a25dc4b1170c',
+    ),
     'psr/cache' => 
     array (
       'pretty_version' => '1.0.1',
@@ -133,6 +187,33 @@ private static $installed = array (
       array (
       ),
       'reference' => '8622567409010282b7aeebe4bb841fe98b58dcaf',
+    ),
+    'psr/http-client' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2dfb5f6c5eff0e91e20e913f8c5452ed95b86621',
+    ),
+    'psr/http-factory' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '12ac7fcd07e5b077433f5f2bee95b3a771bf61be',
+    ),
+    'psr/http-message' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f6561bf28d520154e4b0ec72be95418abe6d9363',
     ),
     'psr/log' => 
     array (
@@ -287,12 +368,12 @@ private static $installed = array (
     ),
     'topthink/think-orm' => 
     array (
-      'pretty_version' => 'v2.0.40',
-      'version' => '2.0.40.0',
+      'pretty_version' => 'v2.0.41',
+      'version' => '2.0.41.0',
       'aliases' => 
       array (
       ),
-      'reference' => '1119d979b850849f3725856460cf108eec1c3eb8',
+      'reference' => '64bbfdde01f4fd6939c2f695fceb08e6d764ac6b',
     ),
     'topthink/think-queue' => 
     array (
